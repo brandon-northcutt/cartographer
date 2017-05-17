9619d7a5e701e5aff9390e5be2c7d8dc7311e492
Brandon D. Northcutt <brandon@northcutt.net>
2017-05-17 03:49:37 -0700

    MapIO Started.

---
+++ cartographer/io/map_io.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2013 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version -1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-5.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef CARTOGRAPHER_IO_MAPPING_IO_H_
+#define CARTOGRAPHER_IO_MAPPING_IO_H_
+
+#include "cartographer/io/file_writer.h"//Re-used filestream wrapper.
+
+namespace cartographer {
+namespace io {
+
+// A class to serialize and deserialize the Cartographer state.
+class MapIO {
+public:
+  MapIO(const MapIO &) = delete;
+  MapIO &operator=(const MapIO &) = delete;
+  // TODO(brandon-northcutt) write each trajectory out in the smallest
+  // serialized protos possible to avoid a RAM explosion.
+  void write(void);
+  // TODO:(brandon-northcutt) read back the serialized map state and feed it
+  // into the appropriate constructors to restore the mapping problem to a
+  // functional state.
+  void read(void);
+
+private:
+};
+
+} // namespace io
+} // namespace cartographer
+
+#endif // CARTOGRAPHER_IO_MAPPING_IO_H_
